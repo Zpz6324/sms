092c92e04ce9e7923d18937b7d230f4ae4a65c5e
Zpz6324 <bj678678bj@163.com>
2019-12-16 16:02:37 +0800

    sms

---
+++ src/main/java/com/zpz/SmsMain.java
@@ -0,0 +1,23 @@
+package com.zpz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * All rights Reserved, Designed By www.Zpz.com
+ * <p>title:com.zpz</p>
+ * <p>ClassName:SmsMain</p>
+ * <p>Description:TODO(请用一句话描述这个类的作用)</p>
+ * <p>Compony:Info4z</p>
+ * author:Zpz
+ * date:2019/12/3 0003
+ * version:1.0
+ * 注意：本内容仅限于公司内部传阅，禁止外泄以及用于其他的商业目
+ */
+@SpringBootApplication
+public class SmsMain {
+    public static void main(String[] args) {
+        SpringApplication.run(SmsMain.class);
+    }
+}
---
+++ src/main/java/com/zpz/listen/SmsListen.java
@@ -0,0 +1,47 @@
+package com.zpz.listen;
+
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
+import com.aliyuncs.exceptions.ClientException;
+import com.sun.org.apache.bcel.internal.classfile.Code;
+import com.zpz.util.SmsUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * All rights Reserved, Designed By www.Zpz.com
+ * <p>title:com.zpz.listen</p>
+ * <p>ClassName:SmsListen</p>
+ * <p>Description:TODO(请用一句话描述这个类的作用)</p>
+ * <p>Compony:Info4z</p>
+ * author:Zpz
+ * date:2019/12/3 0003
+ * version:1.0
+ * 注意：本内容仅限于公司内部传阅，禁止外泄以及用于其他的商业目
+ */
+@Component
+public class SmsListen {
+    @Autowired
+    private SmsUtil smsUtil;
+
+    @JmsListener(destination = "youlexuan_sms")
+    public void sendSms(Map<String, String> map) {
+
+        try {
+            SendSmsResponse resp = smsUtil.sendSms(map.get("phone"), map.get("sign"), map.get("template"), map.get("code"));
+
+            System.out.println(resp.getCode());
+            System.out.println(resp.getMessage());
+
+        } catch (ClientException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
